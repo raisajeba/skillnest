ec95eef2966f8d1b61d8eba3be4c8dd5f4e3f335
Jeba Raisa <raisajeba8@gmail.com>
2026-04-30 18:13:22 +0600

    Added Skill models and registered them in admin

--- skills/admin.py
+++ skills/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Skill,Category,SkillImage
+
+class SkillImageInline(admin.TabularInline):
+    model = SkillImage
+    extra = 1  # default
+
+class SkillAdmin(admin.ModelAdmin):
+    inlines = [SkillImageInline]
+
+admin.site.register(Category)
+admin.site.register(Skill,SkillAdmin)
+
 
 # Register your models here.
--- skills/models.py
+++ skills/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Category(models.Model):
+    name = models.CharField(max_length= 50)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
+class Skill(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+class SkillImage(models.Model):
+    skill = models.ForeignKey(Skill, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='skill_pics/')
+
+    def __str__(self):
+        return f"Image for {self.skill.name}"
